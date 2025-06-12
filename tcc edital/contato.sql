bce97c777f248437712aedc1b56c66a4ae8d43f7
suzan <suzan.monteiro@etec.sp.gov.br>
2025-06-12 16:27:26 -0300

    subindo o site para oo

---
+++ tcc edital/contato.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS entrelinhas;
+USE entrelinhas;
+
+CREATE TABLE contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    comentario TEXT NOT NULL,
+    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
