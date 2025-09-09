aad56b4254da0192e8e4e41cca89a43664cd51c4
Mayara8666 <mayara.santana46@etec.sp.gov.br>
2025-09-09 17:56:49 -0300

    sobre-nos

--- entrelinhas_php_final/conteudo/sobre.php
+++ entrelinhas_php_final/conteudo/sobre.php
@@ -1,32 +1,44 @@
- <div>
-    <div class="identificar-card">
-      <img src="" alt="">
-      <h3>Mayara de O. Santana</h3>
-      <p>Desenvolvedora. Atuou de forma ampla no projeto, sendo responsável pela conexão com o banco de dados do site, pela estruturação e design da documentação, além da implementação do site. Desenvolveu as seções "Sobre Nós" e "Início", organizando o conteúdo e garantindo a usabilidade. Também contribuiu na organização geral do projeto e na padronização das informações.</p> 
-    </div>
-    <div class="identificar-card">
-      <h3>Suzan Marry F. Monteiro</h3>
-      <p>Responsável pelo design do site, incluindo a implementação do modo escuro e da funcionalidade "ler tudo", voltada para acessibilidade de pessoas com dislexia. Contribuiu com a pesquisa de leis e direitos relacionados ao tema do projeto e desenvolveu, em conjunto com a Emilly, o formulário para a pesquisa de campo. Além disso, atuou no gerenciamento das redes sociais, sendo responsável pelo Twitter do site em parceria com a Emilly.</p>
-    </div>
-    <div class="identificar-card">
-      <h3>Emilly Magela </h3>
-      <p>Contribuiu para a elaboração do formulário da pesquisa de campo, participando também da definição das abas do site. Foi responsável por registrar na documentação do projeto os resultados obtidos com a pesquisa. Implementou a fonte OpenDyslexic no site, trazendo mais acessibilidade, e atuou na gestão das redes sociais, cuidando do Instagram do projeto. Além disso, pesquisou sobre aplicativos que auxiliam pessoas com dislexia e realizou estudos sobre a história e inspirações relacionadas ao tema.</p>
-    </div>
-    <div class="identificar-card">
-      <h3>Paula G. Faustino </h3>
-      <p>Foi responsável por trazer materiais gratuitos e conceitos sobre dislexia para compor o conteúdo do site. Descobriu uma fonte acessível para auxiliar pessoas com dislexia e colaborou ativamente na documentação do projeto, contribuindo com as imagens, a conclusão, o objetivo e o resumo do produto, além de criar a imagem de capa.</p>
-    </div>
-</div>
+
 <section id="sobre-nos">
   <h2>Entre Linhas<h2>
 <p>O Entre Linhas é essencial para criar laços com pessoas que buscam conhecimento, familiares, educadores ou até mesmo quem convive com a dislexia. Conteúdos claros e bem elaborados têm o poder de inspirar compreensão, empatia e oferecer apoio para quem precisa. Nosso objetivo é informar e conscientizar, tornando a dislexia um tema de prioridade e de fácil acesso para todos.</p>
 <p> Faça do Entre Linhas uma bela representação da inclusão, trazendo informações confiáveis, conteúdos educativos e exemplos reais que ajudam a entender melhor o que é a dislexia! Você quer receber materiais que desmistificam este transtorno de aprendizagem? Deseja aprender maneiras de apoiar amigos, filhos ou alunos com dislexia? Ou gostaria de conhecer histórias inspiradoras de pessoas que convivem com a condição? As possibilidades são infinitas, e todas têm como foco ampliar o conhecimento e transformar a forma como enxergamos a dislexia!</p>
 </section>
+
+<div style="display: flex; gap: 20px; flex-wrap: wrap; justify-content: center; padding-top: 50px;">
+    <div class="identificar-card">
+      <h3>Mayara de O. Santana</h3>
+       <img src="may.jpeg" alt="grupo" class="logo-img">
+      <p>Desenvolvedora. Atuou de forma ampla no projeto, sendo responsável pela conexão com o banco de dados do site, pela estruturação e design da documentação, além da implementação do site. Desenvolveu as seções "Sobre Nós" e "Início", organizando o conteúdo e garantindo a usabilidade. Também contribuiu na organização geral do projeto e na padronização das informações.</p> 
+    </div>
+    <div class="identificar-card">
+      <h3>Suzan Marry F. Monteiro</h3>
+      <img src="suzan.jpeg" alt="grupo" class="logo-img">
+      <p>Responsável pelo design do site, incluindo a implementação do modo escuro e da funcionalidade "ler tudo", voltada para acessibilidade de pessoas com dislexia. Contribuiu com a pesquisa de leis e direitos relacionados ao tema do projeto e desenvolveu, em conjunto com a Emilly, o formulário para a pesquisa de campo. Além disso, atuou no gerenciamento das redes sociais, sendo responsável pelo Twitter do site em parceria com a Emilly.</p>
+    </div>
+    <div class="identificar-card">
+      <h3>Emilly Magela </h3>
+      <img src="emilly.jpeg" alt="grupo" class="logo-img">
+      <p>Contribuiu para a elaboração do formulário da pesquisa de campo, participando também da definição das abas do site. Foi responsável por registrar na documentação do projeto os resultados obtidos com a pesquisa. Implementou a fonte OpenDyslexic no site, trazendo mais acessibilidade, e atuou na gestão das redes sociais, cuidando do Instagram do projeto. Além disso, pesquisou sobre aplicativos que auxiliam pessoas com dislexia e realizou estudos sobre a história e inspirações relacionadas ao tema.</p>
+    </div>
+    <div class="identificar-card">
+      <h3>Paula G. Faustino </h3>
+      <img src="papa.jpeg" alt="grupo" class="logo-img">
+      <p>Foi responsável por trazer materiais gratuitos e conceitos sobre dislexia para compor o conteúdo do site. Descobriu uma fonte acessível para auxiliar pessoas com dislexia e colaborou ativamente na documentação do projeto, contribuindo com as imagens, a conclusão, o objetivo e o resumo do produto, além de criar a imagem de capa.</p>
+    </div>
+</div>
+
+
 <section>
   <h2>Nosso objetivo</h2>
   <p>O Entre Linhas tem como objetivo informar e conscientizar sobre a dislexia, apresentando suas características, causas e impactos no aprendizado. Buscamos oferecer exemplos práticos, materiais gratuitos e acessíveis, além de histórias inspiradoras, que promovem empatia e compreensão. Também mapeamos recursos e informações úteis, incluindo profissionais especializados, leis e direitos, para apoiar famílias, educadores, alunos e toda a comunidade no processo de inclusão e no entendimento da dislexia.</p>
 </section>
  
+<section>
+  <h2>Redes Sociais</h2>
+  <p>Instagram-https://www.instagram.com/entrelinhassite?igsh=eHNzMWFlb2RzY2J3</p> 
+  <p></p>
+
 <button class="dark-toggle">Modo Escuro</button>
   <script>
     const toggle = document.querySelector('.dark-toggle');
--- entrelinhas_php_final/entrelinhas_php_final/entrelinhas_php_final/contato.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS entrelinhas;
-USE entrelinhas;
-
-CREATE TABLE contatos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    comentario TEXT NOT NULL,
-    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
